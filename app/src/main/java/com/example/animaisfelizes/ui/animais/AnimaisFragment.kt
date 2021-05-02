d80477d5a4aebcee322b03fb79dc67e9b66901dc
Matheus Souza <matheusouzaaaa@gmail.com>
2021-05-02 04:34:21 -0300

    Funcionalidade INSERT desenvolvida.

--- app/src/main/java/com/example/animaisfelizes/ui/animais/AnimaisFragment.kt
+++ app/src/main/java/com/example/animaisfelizes/ui/animais/AnimaisFragment.kt
@@ -1,20 +1,75 @@
 package com.example.animaisfelizes.ui.animais
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.animaisfelizes.R
+import com.example.animaisfelizes.data.db.AppDatabase
+import com.example.animaisfelizes.data.db.dao.AnimalDAO
+import com.example.animaisfelizes.extension.hideKeyboard
+import com.example.animaisfelizes.repository.AnimalRepository
+import com.example.animaisfelizes.repository.DatabaseDataSource
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.animais_fragment.*
 
-class AnimaisFragment : Fragment() {
+class AnimaisFragment : Fragment(R.layout.animais_fragment) {
 
-    private lateinit var viewModel: AnimaisViewModel
+    private val viewModel: AnimaisViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val animalDAO: AnimalDAO = AppDatabase.getInstance(requireContext()).animalDAO
+                val repository: AnimalRepository = DatabaseDataSource(animalDAO)
+                return AnimaisViewModel(repository) as T
+            }
+        }
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.animais_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeEvents()
+        setListeners()
+    }
+
+    private fun observeEvents() {
+        viewModel.animalStateEventData.observe(viewLifecycleOwner){ animalState ->
+            when (animalState){
+                is AnimaisViewModel.AnimalState.Inserted -> {
+                    clearFields()
+                    hideKeyboard()
+                }
+            }
+        }
+
+        viewModel.messageStateEventData.observe(viewLifecycleOwner) { stringResId ->
+            Snackbar.make(requireView(), stringResId, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun clearFields() {
+        input_name.text?.clear()
+        input_idade.text?.clear()
+        input_proprietario.text?.clear()
+    }
+
+    private fun hideKeyboard() {
+        val parentActivity = requireActivity()
+        if(parentActivity is AppCompatActivity){
+            parentActivity.hideKeyboard()
+        }
+    }
+
+    private fun setListeners() {
+        button_animal.setOnClickListener {
+            val nome = input_name.text.toString()
+            val idade = input_idade.text.toString()
+            val proprietario = input_proprietario.text.toString()
+
+            viewModel.addAnimal(nome, idade, proprietario)
+        }
     }
 }
--- app/src/main/java/com/example/animaisfelizes/ui/animais/AnimaisViewModel.kt
+++ app/src/main/java/com/example/animaisfelizes/ui/animais/AnimaisViewModel.kt
@@ -1,7 +1,45 @@
 package com.example.animaisfelizes.ui.animais
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.animaisfelizes.R
+import com.example.animaisfelizes.repository.AnimalRepository
+import kotlinx.coroutines.launch
 
-class AnimaisViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AnimaisViewModel(private val repository: AnimalRepository) : ViewModel() {
+
+    private val _animalStateEventData = MutableLiveData<AnimalState>()
+    val animalStateEventData: LiveData<AnimalState>
+        get() = _animalStateEventData
+
+    private val _messageEventData = MutableLiveData<Int>()
+    val messageStateEventData: LiveData<Int>
+        get() = _messageEventData
+
+
+    fun addAnimal(nome: String, idade: String, proprietario: String) = viewModelScope.launch {
+        try{
+            val id = repository.insertAnimal(nome,idade,proprietario)
+
+            if(id > 0){
+                //foi inserido
+                _animalStateEventData.value = AnimalState.Inserted
+                _messageEventData.value = R.string.animal_inserido_sucesso
+            }
+        } catch (ex: Exception){
+            _messageEventData.value = R.string.animal_inserido_erro
+            Log.e(TAG,ex.toString())
+        }
+    }
+
+    sealed class AnimalState{
+        object Inserted: AnimalState()
+    }
+
+    companion object {
+        private val TAG = AnimaisViewModel::class.java.simpleName
+    }
 }
